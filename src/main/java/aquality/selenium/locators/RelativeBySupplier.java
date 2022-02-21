2090231d9125547d36526310dd70cd88cff70760
Andrei Pavar <a.pavar@yandex.ru>
2022-02-21 12:41:42 +0300

    [Feature/Add_Relative_Locators] Add private constructor for RelativeBySupplier class

--- src/main/java/aquality/selenium/locators/RelativeBySupplier.java
+++ src/main/java/aquality/selenium/locators/RelativeBySupplier.java
@@ -3,6 +3,10 @@ package aquality.selenium.locators;
 import org.openqa.selenium.By;
 
 public class RelativeBySupplier {
+
+    private RelativeBySupplier() {
+    }
+
     public static RelativeBy with(By by) {
         return new RelativeBy(by);
     }
