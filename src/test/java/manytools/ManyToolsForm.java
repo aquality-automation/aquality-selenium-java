1af97842573f3761a5530ec0f6ec0139fbcbe597
Aliaksej Mialeshka <a2.meleshko@a1qa.com>
2025-05-29 11:56:07 +0200

    handle cookies for manytools

--- src/test/java/manytools/ManyToolsForm.java
+++ src/test/java/manytools/ManyToolsForm.java
@@ -2,6 +2,7 @@ package manytools;
 
 import aquality.selenium.browser.AqualityServices;
 import aquality.selenium.core.utilities.IActionRetrier;
+import aquality.selenium.elements.interfaces.IButton;
 import aquality.selenium.elements.interfaces.ILabel;
 import aquality.selenium.forms.Form;
 import org.openqa.selenium.By;
@@ -12,6 +13,7 @@ import java.util.Collections;
 public abstract class ManyToolsForm<T extends ManyToolsForm<T>> extends Form {
     private static final String BASE_URL = "https://manytools.org/";
     private final ILabel lblValue = getFormLabel().findChildElement(By.xpath(".//code"), getName(), ILabel.class);
+    private final IButton btnAgree = getElementFactory().getButton(By.xpath("//button[@mode='primary']"), "Agree");
 
     protected ManyToolsForm(String name) {
         super(By.id("maincontent"), name);
@@ -24,6 +26,11 @@ public abstract class ManyToolsForm<T extends ManyToolsForm<T>> extends Form {
         AqualityServices.get(IActionRetrier.class).doWithRetry(() -> {
             AqualityServices.getBrowser().goTo(BASE_URL + getUrlPart());
             AqualityServices.getBrowser().waitForPageToLoad();
+            if (btnAgree.state().isDisplayed())
+            {
+                btnAgree.click();
+                btnAgree.state().waitForNotDisplayed();
+            }
         }, Collections.singletonList(TimeoutException.class));
         return (T) this;
     }
