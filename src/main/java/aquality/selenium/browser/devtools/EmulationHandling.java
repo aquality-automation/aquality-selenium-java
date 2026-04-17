be5638cb3c743041331d664aa409435de7e4fcc2
Aleksey Meleshko <a2.meleshko@a1qa.com>
2026-04-17 14:24:22 +0200

    fix javadocs issue

--- src/main/java/aquality/selenium/browser/devtools/EmulationHandling.java
+++ src/main/java/aquality/selenium/browser/devtools/EmulationHandling.java
@@ -146,18 +146,18 @@ public class EmulationHandling {
      * @param height                         Value to override window.screen.height
      * @param deviceScaleFactor              Overriding device scale factor value. 0 disables the override.
      * @param mobile                         Whether to emulate mobile device. This includes viewport meta tag, overlay scrollbars, text auto-sizing and more.
-     * @param scale                          Scale to apply to resulting view image. Ignored in |dontSetVisibleSize| is not set.
-     * @param screenWidth                    Value to override window.screen.width. Ignored in |dontSetVisibleSize| is not set.
-     * @param screenHeight                   Value to override window.screen.height. Ignored in |dontSetVisibleSize| is not set.
-     * @param positionX                      Overriding view X position on screen in device independent pixels (dip). Ignored in |dontSetVisibleSize| is not set.
-     * @param positionY                      Overriding view Y position on screen in device independent pixels (dip). Ignored in |dontSetVisibleSize| is not set.
-     * @param dontSetVisibleSize             Whether to not set visible view size, rely upon explicit setVisibleSize call. Ignored in |scale| is not set.
-     * @param screenOrientation              Orientation of the screen. This is ignored in |dontSetVisibleSize| is not set.
-     * @param viewport                       If set, the visible area of the overridden device screen, not affecting the reported screen size. Ignored in |dontSetVisibleSize| is not set.
-     * @param displayFeature                 Configuration of the display when the system is in unified mode (e.g. foldable devices).
-     * @param devicePosture                  The posture of the device (e.g. foldable devices).
-     * @param scrollbarType                  The type of the scrollbars to render (e.g. mobile vs desktop). Ignored in |dontSetVisibleSize| is not set.
-     * @param screenOrientationLockEmulation Whether to emulate a focused form control that would cause the virtual keyboard to pop up. Ignored in |dontSetVisibleSize| is not set.
+     * @param scale                          Scale to apply to resulting view image. Ignored if |dontSetVisibleSize| is set.
+     * @param screenWidth                    Value to override window.screen.width. Ignored if |dontSetVisibleSize| is set.
+     * @param screenHeight                   Value to override window.screen.height. Ignored if |dontSetVisibleSize| is set.
+     * @param positionX                      Overriding view X position on screen in device independent pixels (dip). Ignored if |dontSetVisibleSize| is set.
+     * @param positionY                      Overriding view Y position on screen in device independent pixels (dip). Ignored if |dontSetVisibleSize| is set.
+     * @param dontSetVisibleSize             Whether to not set visible view size, rely upon explicit setVisibleSize call. Ignored if |scale| is set.
+     * @param screenOrientation              Orientation of the screen. Ignored if |dontSetVisibleSize| is set.
+     * @param viewport                       If set, the visible area of the overridden device screen, not affecting the reported screen size. Ignored if |dontSetVisibleSize| is set.
+     * @param displayFeature                 Configuration of the display when the system is in unified mode (e.g., foldable devices).
+     * @param devicePosture                  The posture of the device (e.g., foldable devices).
+     * @param scrollbarType                  The type of the scrollbars to render (e.g., mobile vs desktop). Ignored if |dontSetVisibleSize| is set.
+     * @param screenOrientationLockEmulation Enables screen orientation lock emulation that intercepts calls to screen.orientation.lock().
      */
     public void setDeviceMetricsOverride(Integer width, Integer height, Number deviceScaleFactor, Boolean mobile, Optional<Number> scale, Optional<Integer> screenWidth, Optional<Integer> screenHeight, Optional<Integer> positionX, Optional<Integer> positionY, Optional<Boolean> dontSetVisibleSize, Optional<ScreenOrientation> screenOrientation, Optional<Viewport> viewport, Optional<DisplayFeature> displayFeature, Optional<DevicePosture> devicePosture, Optional<Emulation.SetDeviceMetricsOverrideScrollbarType> scrollbarType, Optional<Boolean> screenOrientationLockEmulation) {
         tools.sendCommand(Emulation.setDeviceMetricsOverride(width, height, deviceScaleFactor, mobile, scale, screenWidth,
