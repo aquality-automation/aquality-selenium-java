d4e3abb31e383d10188773657840faa4ddd202be
Aleksey Meleshko <a2.meleshko@a1qa.com>
2026-07-29 12:47:38 +0200

    Stabilize ManyTools site navigation and waits

--- src/test/java/manytools/ManyToolsForm.java
+++ src/test/java/manytools/ManyToolsForm.java
@@ -1,6 +1,7 @@
 package manytools;
 
 import aquality.selenium.browser.AqualityServices;
+import aquality.selenium.core.elements.ElementState;
 import aquality.selenium.core.utilities.IActionRetrier;
 import aquality.selenium.elements.interfaces.IButton;
 import aquality.selenium.elements.interfaces.ILabel;
@@ -12,8 +13,9 @@ import java.util.Collections;
 
 public abstract class ManyToolsForm<T extends ManyToolsForm<T>> extends Form {
     private static final String BASE_URL = "https://manytools.org/";
-    private final ILabel lblValue = getFormLabel().findChildElement(By.xpath(".//code"), getName(), ILabel.class);
-    private final IButton btnAgree = getElementFactory().getButton(By.xpath("//button[@mode='primary']"), "Agree");
+    private final ILabel lblValue = getFormLabel().findChildElement(By.xpath(".//code"), getName(), ILabel.class, ElementState.EXISTS_IN_ANY_STATE);
+    private final ILabel lblConsentDialog = getElementFactory().getLabel(By.id("cmpwrapper"), "Cookie consent dialog", ElementState.EXISTS_IN_ANY_STATE);
+    private final IButton btnDecline = lblConsentDialog.findElementInShadowRoot(By.id("cmpbntnotxt"), "Decline cookies", IButton.class);
 
     protected ManyToolsForm(String name) {
         super(By.id("maincontent"), name);
@@ -25,7 +27,13 @@ public abstract class ManyToolsForm<T extends ManyToolsForm<T>> extends Form {
     public T open() {
         AqualityServices.get(IActionRetrier.class).doWithRetry(() -> {
             AqualityServices.getBrowser().goTo(BASE_URL + getUrlPart());
+            state().waitForDisplayed();
         }, Collections.singletonList(TimeoutException.class));
+
+        if (lblConsentDialog.state().isExist() && btnDecline.state().isDisplayed()) {
+            btnDecline.click();
+            lblConsentDialog.state().waitForNotDisplayed();
+        }
         return (T) this;
     }
 
